67c148886ce01ac1913291227094f8b2158b11be
zhaokun <283798272@qq.com>
2021-11-23 15:33:42 +0800

    数据库文档生成插件demo

---
+++ src/main/java/com/fortunebill/screw/ScrewDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fortunebill.screw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrewDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrewDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fortunebill/screw/ScrewDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortunebill.screw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrewDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
